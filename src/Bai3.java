89355e9e1300413dc28c8c6d62c60c3f8a71df2e
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-01-28 22:08:33 +0700

    them bai 3

---
+++ src/Bai3.java
@@ -0,0 +1,14 @@
+public class Bai3 {
+    public static void main(String[] args) {
+        String tenSach = "Tu hoc tai nha";
+        int namXuatBan = 2023;
+        double giaThanh = 12.5;
+        boolean conSach = false;
+        char maKho = 'A';
+        System.out.println("Ten sach: "+ tenSach);
+        System.out.println("Nam xuat ban: "+namXuatBan);
+        System.out.println("Gia thanh: "+giaThanh);
+        System.out.println("Tinh trang: "+ (conSach?"Con sach": "Het sach"));
+        System.out.println("Ma kho: "+ maKho);
+    }
+}

66b905fa68453932c847d59084bb9f42e58f47ed
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-01-28 21:54:48 +0700

    them bai 2

---
+++ src/Bai2.java
@@ -0,0 +1,10 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String loiChao = "Hello";
+        String name = "Mai Thanh Binh";
+        String lopHoc;
+        lopHoc = "22DTH5";
+        System.out.println(loiChao +" "+ name + " Lop " + lopHoc);
+
+    }
+}

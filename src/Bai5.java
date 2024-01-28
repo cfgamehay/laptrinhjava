d3fa691a6c37765cfbf899799dc199cc349aa69c
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-01-28 22:35:38 +0700

    Them bai 5 (chu thich)

---
+++ src/Bai5.java
@@ -0,0 +1,35 @@
+/**
+ * @author Binh
+ */
+public class Bai5 {
+    public static void main(String[] args) {
+        /**
+         * @param args: tham so dau vao
+         *
+         */
+        //Khai bao bien
+        int x= 10;
+        /*
+        Sysout -> xuat ra man hinh
+         */
+        System.out.println(x);
+
+
+
+
+
+        //ghi chu 1 dong
+        /*
+        ghi chu
+        nhieu dong
+        */
+
+        /**
+         * ghi chu
+         * de huong dan
+         * :)
+         */
+        // ghi chu dung de ghi chu hoac vo hieu hoa code
+
+    }
+}

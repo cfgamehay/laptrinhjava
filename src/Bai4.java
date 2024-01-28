ad2fe9679772e0e109538e77d8c2ad9eb2e3e2be
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-01-28 22:25:48 +0700

    Them bai 4

---
+++ src/Bai4.java
@@ -0,0 +1,7 @@
+public class Bai4 {
+    public static void main(String[] args) {
+        final int namSinh = 2004;
+        //namSinh = 2020; khong thay doi dc
+        System.out.println("Nam sinh: "+ namSinh);
+    }
+}

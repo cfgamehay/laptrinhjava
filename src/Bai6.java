ac35eb3ef380e7be933d5a5ffee4e036ddf72b7b
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-01-28 22:42:53 +0700

    Them bai 6 (kiểm tra và xử lý lỗi biên dịch)

---
+++ src/Bai6.java
@@ -0,0 +1,16 @@
+public class Bai6 {
+    public static void main(String[] args) {
+        //neu ten class khong giong ten file thi se khong the chay file
+        System.out.println("Xin chao");
+
+        //khi dung bien chua khai bao
+        //System.out.println(x);
+
+        final int x = 9;
+        System.out.println(x);
+        //x=10; bien x la hang so va khong the thay doi
+
+        //thieu dấu cũng gây lỗi -> syntax error
+
+    }
+}

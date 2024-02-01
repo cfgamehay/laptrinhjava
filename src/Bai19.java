f105aa2812fb8e9d583c1e9f7db866e777f4036e
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-02-01 23:04:30 +0700

    them bai 19

---
+++ src/Bai19.java
@@ -0,0 +1,13 @@
+public class Bai19 {
+    public static void main(String[] args) {
+        for (int i = 0;i<2000;i++)
+        {
+            System.out.println( (i+1)+ " " + "em hua se lam bai tap day du");
+        }
+
+        for (int i = 2000;i>0;i--)
+        {
+            System.out.println( (i)+ " " + "em hua se lam bai tap day du");
+        }
+    }
+}

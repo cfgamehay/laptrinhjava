721895b58501e1af9e3a51b186a623f16ab3edb9
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-02-02 00:01:02 +0700

    Them bai 23

---
+++ src/Bai23.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bai23 {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+            System.out.println("Nhap so lon hon 0: ");
+            x = sc.nextInt();
+
+        }while (x<=0);
+    }
+}

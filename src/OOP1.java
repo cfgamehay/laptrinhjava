a684ed82dc25674d674950de55d8bded6ba97cac
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-02-02 23:55:20 +0700

    Them OOP bai 1

---
+++ src/OOP1.java
@@ -0,0 +1,9 @@
+public class OOP1 {
+    public static void main(String[] args) {
+        MyDate date = new MyDate(10,2,2024);
+        date.printDay();
+        date.printMonth();
+        date.printYear();
+        date.printDate();
+    }
+}

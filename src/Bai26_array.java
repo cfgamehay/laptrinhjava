b8d3b2fb5495794784940b9e7ef80b550303696f
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-02-02 23:26:19 +0700

    them bai 26 array

---
+++ src/Bai26_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bai26_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //cach khai bao
+        double[] mang1;
+        double mang2[];
+//        mang1 = new double[10];
+//        for(int i = 0; i<mang1.length;i++)
+//        {
+//            System.out.println("Nhap phan tu thu "+ i+": ");
+//            mang1[i] = sc.nextInt();
+//        }
+//        System.out.println("So phan tu: "+ mang1.length);
+        mang2 = new double[]{1,2,3,4,5};
+        for (int i = 0; i < mang2.length; i++) {
+            System.out.println(mang2[i]);
+        }
+    }
+
+
+}

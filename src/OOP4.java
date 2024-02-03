b56ca4146229b4743816a99e060169e1d3c8f0bb
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2024-02-03 23:02:00 +0700

    Them bai OOP 4

---
+++ src/OOP4.java
@@ -0,0 +1,11 @@
+public class OOP4 {
+    public static void main(String[] args) {
+        MyDate3 md1 = new MyDate3(15, 5, 2025);
+        MyDate3 md2 = new MyDate3(11, 1, 2021);
+        MyDate3 md3 = new MyDate3(30, 3, 2030);
+
+        System.out.println(md1);
+        System.out.println(md2);
+        System.out.println(md3);
+    }
+}
